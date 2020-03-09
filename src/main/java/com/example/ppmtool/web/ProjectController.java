01789b1a6e5a84ad6c05a071bae3bad470962b0b
Jeena <jeenathampi.23@gmail.com>
2020-03-09 12:37:57 -0700

    validation error mapping

--- src/main/java/com/example/ppmtool/web/ProjectController.java
+++ src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -1,14 +1,19 @@
 package com.example.ppmtool.web;
 
 import com.example.ppmtool.domain.Project;
+import com.example.ppmtool.services.MapValidationErrorService;
 import com.example.ppmtool.services.ProjectServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +22,15 @@ public class ProjectController {
     @Autowired
     private ProjectServices projectServices;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
+        if(errorMap!=null) return errorMap;
+
         Project project1 = projectServices.saveOrUpdateProject(project);
 
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
