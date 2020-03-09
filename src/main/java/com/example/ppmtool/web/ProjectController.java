3e76279a8c57a0976bbb6a70826568f40e68b78a
Jeena <jeenathampi.23@gmail.com>
2020-03-09 16:45:26 -0700

    added find project by project identifier

--- src/main/java/com/example/ppmtool/web/ProjectController.java
+++ src/main/java/com/example/ppmtool/web/ProjectController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.*;
@@ -34,5 +31,11 @@ public class ProjectController {
         Project project1 = projectServices.saveOrUpdateProject(project);
 
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> findProject(@PathVariable String projectId){
+        Project project1 = projectServices.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project1, HttpStatus.FOUND);
     }
 }
