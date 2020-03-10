dc5e712c795080c2d1c2c065e7bc30e5f5acb9e1
Jeena <jeenathampi.23@gmail.com>
2020-03-10 15:42:47 -0700

    added dashboard and project item components

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="container">
+          <a className="navbar-brand" href="#">
+            Personal Project Management Tool
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+            <button
+              style={{ margin: "0px 10px 0px 0px" }}
+              className="btn btn-outline-light"
+              type="submit"
+            >
+              Register
+            </button>
+            <button className="btn btn-outline-light" type="submit">
+              Login
+            </button>
+          </div>
+        </nav>
+      </nav>
+    );
+  }
+}
+
+export default Header;
