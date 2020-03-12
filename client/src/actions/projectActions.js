625281854f30b709f867ffc20f3ffceb6f6c78af
Jeena <jeenathampi.23@gmail.com>
2020-03-12 12:18:55 -0700

    added delete feature

--- client/src/actions/projectActions.js
+++ client/src/actions/projectActions.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("/api/project", project);
 
     history.push("/dashboard");
+    dispatch({ type: GET_ERRORS, payload: {} });
   } catch (error) {
     dispatch({ type: GET_ERRORS, payload: error.response.data });
   }
@@ -17,8 +18,24 @@ export const getProjects = () => async dispatch => {
   dispatch({ type: GET_PROJECTS, payload: res.data });
 };
 
-export const getProject = id => async dispatch => {
-  const res = await axios.get(`/api/project/${id}`);
+export const getProject = (id, history) => async dispatch => {
+  try {
+    const res = await axios.get(`/api/project/${id}`);
 
-  dispatch({ type: GET_PROJECT, payload: res.data });
+    dispatch({ type: GET_PROJECT, payload: res.data });
+  } catch (error) {
+    history.push("/dashboard");
+  }
+};
+
+export const deleteProject = (id, history) => async dispatch => {
+  if (
+    window.confirm(
+      "Please confirm to delete the project and all the related tasks"
+    )
+  ) {
+    await axios.delete(`/api/project/${id}`);
+
+    dispatch({ type: DELETE_PROJECT, payload: id });
+  }
 };
