03a692f044952b982cb920fdbeda7202418559b4
Jeena <jeenathampi.23@gmail.com>
2020-03-16 22:41:25 -0700

    added project task features read and update

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,11 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import { Provider } from "react-redux";
 import store from "./store";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 
 function App() {
   return (
@@ -17,6 +20,13 @@ function App() {
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/addproject" component={AddProject} />
         <Route exact path="/updateproject/:id" component={UpdateProject} />
+        <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+        <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
+        <Route
+          exact
+          path="/updateProjectTask/:id/:project_sequence"
+          component={UpdateProjectTask}
+        />
       </Router>
     </Provider>
   );
