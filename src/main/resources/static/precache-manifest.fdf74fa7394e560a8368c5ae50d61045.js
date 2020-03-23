388897f3b09c9d6f77f5c14779affb5ee1c6ee56
Jeena <jeenathampi.23@gmail.com>
2020-03-22 19:44:25 -0700

    added react code

---
+++ src/main/resources/static/precache-manifest.fdf74fa7394e560a8368c5ae50d61045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19e901614d350d59ca0a6519fd3d0c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d7c3062c4fd29fd651",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "067d84d7f51fc7334811",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7d7c3062c4fd29fd651",
+    "url": "/static/js/2.15aeeb01.chunk.js"
+  },
+  {
+    "revision": "60daf3ca32e3efe3fd357caa9175a975",
+    "url": "/static/js/2.15aeeb01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067d84d7f51fc7334811",
+    "url": "/static/js/main.8d1318e2.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
